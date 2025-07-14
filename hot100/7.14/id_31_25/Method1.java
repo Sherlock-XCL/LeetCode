3e6ec5ef2acdb7d927354fd59afed35bf45d4b9d
Sherlock_XCL <1428187976@qq.com>
2025-07-14 11:15:20 +0800

    id_31_25（K 个一组翻转链表）

---
+++ hot100/7.14/id_31_25/Method1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Method1 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        List<Integer> a = new ArrayList<>();
+        ListNode cur = head;
+        int cnt = 0, i = 0;
+        while (cur != null) {
+            cnt ++;
+            a.add(cur.val);
+            if (cnt % k == 0) {
+                // 反转
+                swap(a, i, cnt - 1);
+                i = cnt;
+            }
+            cur = cur.next;
+        }
+        ListNode x = new ListNode(0);
+        ListNode ans = x;
+        for (int aa : a) {
+            ans.next = new ListNode(aa);
+            ans = ans.next;
+        }
+        return x.next;
+    }
+    // 1 2 3 4 5 6 7
+    //       i     cnt
+    // i=3    cnt=6
+    private void swap(List<Integer> a, int start, int end) {
+        while (start < end) {
+            int tmp = a.get(start); // 值
+            a.set(start, a.get(end));
+            a.set(end, tmp);
+            start ++;
+            end --;
+        }
+    }
+    
+}
