0739ed94186b456601b73f8804e6d93ac332559a
Sherlock_XCL <1428187976@qq.com>
2025-07-14 10:26:18 +0800

    id_39_24（两两交换链表中的节点）

---
+++ 24.两两交换链表中的节点.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        List<Integer> a = new ArrayList<>();
+        ListNode cur = head, pre = null;
+        int cnt = 0;
+        while (cur != null) {
+            cnt ++;
+            if (cnt % 2 == 0) {
+                // 先删数组最后的元素
+                a.remove(a.size() - 1);
+                a.add(cur.val);
+                a.add(pre.val);        
+            } else {
+                a.add(cur.val);
+            }
+            pre = cur;
+            cur = cur.next;
+            
+        }
+        ListNode k = new ListNode(0);
+        ListNode ans = k;
+        for (int x : a) {
+            ans.next = new ListNode(x);
+            ans = ans.next; 
+        }
+        return k.next;
+    }
+}
+// @lc code=end
+
---
+++ hot100/7.14/id_39_24/Method1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Method1 {
+    public ListNode swapPairs(ListNode head) {
+        List<Integer> a = new ArrayList<>();
+        ListNode cur = head, pre = null;
+        int cnt = 0;
+        while (cur != null) {
+            cnt ++;
+            if (cnt % 2 == 0) {
+                // 先删数组最后的元素
+                a.remove(a.size() - 1);
+                a.add(cur.val);
+                a.add(pre.val);        
+            } else {
+                a.add(cur.val);
+            }
+            pre = cur;
+            cur = cur.next;
+            
+        }
+        ListNode k = new ListNode(0);
+        ListNode ans = k;
+        for (int x : a) {
+            ans.next = new ListNode(x);
+            ans = ans.next; 
+        }
+        return k.next;
+    }
+}
