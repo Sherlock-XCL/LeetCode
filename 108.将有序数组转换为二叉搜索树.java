aed4a26aa0041de711e38d135cb3f5ec534866e9
Sherlock_XCL <1428187976@qq.com>
2025-07-23 21:27:06 +0800

    id_42_108（将有序数组转换为二叉搜索树）

---
+++ 108.将有序数组转换为二叉搜索树.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=java
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+    private TreeNode dfs(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int mid = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, l, mid - 1);
+        root.right = dfs(nums, mid + 1, r);
+        return root;
+    }
+}
+// @lc code=end
+
---
+++ hot100/7.23/id_42_108/Method1.java
@@ -0,0 +1,27 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Method1 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+    private TreeNode dfs(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int mid = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, l, mid - 1);
+        root.right = dfs(nums, mid + 1, r);
+        return root;
+    }
+}
