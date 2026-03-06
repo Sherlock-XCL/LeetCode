195a122ad2e2beb285571b8e42304c3af3e660b7
Sherlock_XCL <Sherlock_XCL@163.com>
2026-03-06 14:14:02 +0800

    复习 hot100-1 两数相加

---
+++ hot100/2026/3.6/id1_1/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                ans[0] = map.get(nums[i]);
+                ans[1] = i;
+            }
+            map.put(target - nums[i], i); // 9 - 2 = 7 (7, 0)
+        }
+        return ans;
+    }
+}
