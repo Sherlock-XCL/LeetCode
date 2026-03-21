e02aa5134c4261b8bb7c01110edebe5fec4ce47b
Sherlock_XCL <Sherlock_XCL@163.com>
2026-03-21 15:47:52 +0800

    复习 hot100-5 盛水最多的容器

--- hot100/2026/3.18/id5_11/Solution.java
+++ hot100/2026/3.18/id5_11/Solution.java
@@ -1,7 +1,13 @@
 class Solution {
     public int maxArea(int[] height) {
-        
-
-        return 0;
+        int l = 0, r = height.length - 1;
+        int ans = -1;
+        while (l < r) {
+            int s = (r - l) * Math.max(height[l], height[r]);
+            ans = Math.max(s, ans);
+            if (height[l] > height[r]) r --;
+            else l ++; 
+        }
+        return ans;
     }
 }
