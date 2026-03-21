13d04645739008a147e4703e6e0d9659717b04b0
Sherlock_XCL <Sherlock_XCL@163.com>
2026-03-21 15:34:16 +0800

    复习 hot100-15 三数之和

--- hot100/2026/3.18/id6_15/Solution.java
+++ hot100/2026/3.18/id6_15/Solution.java
@@ -1,11 +1,18 @@
-/*
- * @lc app=leetcode.cn id=15 lang=java
- *
- * [15] 三数之和
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-// @lc code=start
-import java.util.*;
+// 三数之和
+// 输入：nums = [-1,0,1,2,-1,-4]。-4 -1 -1 0 1 2
+// 输出：[[-1,-1,2],[-1,0,1]]      i  j        k
+// 解释：
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+// 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+// 注意，输出的顺序和三元组的顺序并不重要。
+
+// 排序 + 双指针
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -21,24 +28,21 @@ class Solution {
                 if (x + nums[i] < 0) { // 不够大 j右移
                     j ++;
                     while (j < k && nums[j] == nums[j - 1]) j ++;
-                } else if (x + nums[i] > 0) {
+                } else if (x + nums[i] < 0) {
                     k --;
-                    while (j < k && nums[k] == nums[k + 1]) k --;
+                    while (j < k && nums[k] == nums[k - 1]) k --;
                 } else {
                     tmp.add(nums[i]);
                     tmp.add(nums[j]);
                     tmp.add(nums[k]);
-                    ans.add(tmp);
                     j ++;
                     k --;
                     while (j < k && nums[j] == nums[j - 1]) j ++;
-                    while (j < k && nums[k] == nums[k + 1]) k --;
+                    while (j < k && nums[k] == nums[k - 1]) k --;
                 }
-                
+                ans.add(tmp);
             }
         }
         return ans;
     }
 }
-// @lc code=end
-
