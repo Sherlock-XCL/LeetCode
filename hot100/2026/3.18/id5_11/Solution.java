bee68dc59263e3bdfb17430800e02626f1897486
Sherlock_XCL <Sherlock_XCL@163.com>
2026-03-21 15:53:30 +0800

    复习 hot100-5 盛水最多的容器

--- hot100/2026/3.18/id5_11/Solution.java
+++ hot100/2026/3.18/id5_11/Solution.java
@@ -1,4 +1,6 @@
 class Solution {
+    // 对于短的一边，中间的任何一条线都无法与他构成更大的容器
+    // 如果要找到比蓝色区域更大的容器，那么肯定不会包含这条线，所以可以直接去掉
     public int maxArea(int[] height) {
         int l = 0, r = height.length - 1;
         int ans = -1;
